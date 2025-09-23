2731fbb8231512fb849f3d4e614158dcbd927954
taipv-angelhack <tai.pham@angelhack.com>
2025-09-23 14:04:35 +0700

    update partner logo ui

--- app/components/home/PartnersSwiper.tsx
+++ app/components/home/PartnersSwiper.tsx
@@ -28,13 +28,13 @@ export default function PartnersSwiper({ logos }: PartnersSwiperProps) {
       >
         {logos.map((logo, index) => (
           <SwiperSlide key={index} className="!w-auto">
-            <div className="flex-shrink-0 flex h-[55px] px-6 flex-col justify-center items-center gap-[10px] rounded-[100px] border-2 border-[#D5D7DA]">
+            <div className="flex-shrink-0 flex w-[200px] h-[55px] px-6 flex-col justify-center items-center gap-[10px] rounded-[100px] border-2 border-[#D5D7DA]">
               <Image
                 src={logo}
                 alt={`Partner Company ${index + 1}`}
                 width={200}
                 height={31}
-                className="w-auto h-[31px] object-contain"
+                className="w-[200px] h-[31px] object-contain"
               />
             </div>
           </SwiperSlide>
